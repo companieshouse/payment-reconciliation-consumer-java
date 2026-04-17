bacca9c906916bf984751e58f86eb58de0ab9fca
dtoulcher-ch <dtoulcher@companieshouse.gov.uk>
2026-04-17 09:23:34 +0100

    [CC-3272] feat: address PR comment regarding TestUtils Class

--- src/test/java/uk/gov/companieshouse/paymentreconciliation/consumer/utils/TestUtils.java
+++ src/test/java/uk/gov/companieshouse/paymentreconciliation/consumer/utils/TestUtils.java
@@ -2,20 +2,11 @@ package uk.gov.companieshouse.paymentreconciliation.consumer.utils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.time.Instant;
 
 import org.jetbrains.annotations.NotNull;
-import org.springframework.http.HttpStatus;
 import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 import payments.payment_processed;
-import uk.gov.companieshouse.api.model.ApiResponse;
-import uk.gov.companieshouse.api.model.payment.PaymentResponse;
-import uk.gov.companieshouse.api.model.payment.RefundModel;
-import uk.gov.companieshouse.api.payments.PaymentDetailsResponse;
 
 public class TestUtils {
 
@@ -24,51 +15,27 @@ public class TestUtils {
     public static final String GET_URI = "/payments/P9hl8PrKRBk1Zmc";
 
     public static String getPaymentResponse() throws IOException {
-        String json = IOUtils.resourceToString("/apiResponses/paymentResponse.json", StandardCharsets.UTF_8);
-        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
-        PaymentResponse paymentResponse = objectMapper.readValue(json, PaymentResponse.class);
-        var apiResponse = new ApiResponse<>(HttpStatus.OK.value(), null, paymentResponse);
-        return objectMapper.writeValueAsString(apiResponse.getData());
+        return IOUtils.resourceToString("/apiResponses/paymentResponse.json", StandardCharsets.UTF_8);
     }
 
     public static String getPaymentRefundResponse() throws IOException {
-        String json = IOUtils.resourceToString("/apiResponses/paymentRefundResponse.json", StandardCharsets.UTF_8);
-        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
-        PaymentResponse paymentResponse = objectMapper.readValue(json, PaymentResponse.class);
-        var apiResponse = new ApiResponse<>(HttpStatus.OK.value(), null, paymentResponse);
-        return objectMapper.writeValueAsString(apiResponse.getData());
+        return IOUtils.resourceToString("/apiResponses/paymentRefundResponse.json", StandardCharsets.UTF_8);
     }
 
     public static String getPaymentDetailsResponse() throws IOException {
-        String json = IOUtils.resourceToString("/apiResponses/paymentDetailsResponse.json", StandardCharsets.UTF_8);
-        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
-        PaymentDetailsResponse paymentResponse = objectMapper.readValue(json, PaymentDetailsResponse.class);
-        var apiResponse = new ApiResponse<>(HttpStatus.OK.value(), null, paymentResponse);
-        return objectMapper.writeValueAsString(apiResponse.getData());
+        return IOUtils.resourceToString("/apiResponses/paymentDetailsResponse.json", StandardCharsets.UTF_8);
     }
 
     public static String getPaymentSensitiveResponse() throws IOException {
-        String json = IOUtils.resourceToString("/apiResponses/paymentSensitiveResponse.json", StandardCharsets.UTF_8);
-        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
-        PaymentResponse paymentResponse = objectMapper.readValue(json, PaymentResponse.class);
-        var apiResponse = new ApiResponse<>(HttpStatus.OK.value(), null, paymentResponse);
-        return objectMapper.writeValueAsString(apiResponse.getData());
+        return IOUtils.resourceToString("/apiResponses/paymentSensitiveResponse.json", StandardCharsets.UTF_8);
     }
 
     public static String getPaymentDetailsSensitiveResponse() throws IOException {
-        String json = IOUtils.resourceToString("/apiResponses/paymentDetailsSensitiveResponse.json", StandardCharsets.UTF_8);
-        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
-        PaymentDetailsResponse paymentResponse = objectMapper.readValue(json, PaymentDetailsResponse.class);
-        var apiResponse = new ApiResponse<>(HttpStatus.OK.value(), null, paymentResponse);
-        return objectMapper.writeValueAsString(apiResponse.getData());
+        return IOUtils.resourceToString("/apiResponses/paymentDetailsSensitiveResponse.json", StandardCharsets.UTF_8);
     }
 
     public static String getLatestRefund() throws IOException {
-        String json = IOUtils.resourceToString("/apiResponses/refundPatchResponse.json", StandardCharsets.UTF_8);
-        ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
-        RefundModel refundModel = objectMapper.readValue(json, RefundModel.class);
-        var apiResponse = new ApiResponse<>(HttpStatus.OK.value(), null, refundModel);
-        return objectMapper.writeValueAsString(apiResponse.getData());
+        return IOUtils.resourceToString("/apiResponses/refundPatchResponse.json", StandardCharsets.UTF_8);
     }
 
     @NotNull
